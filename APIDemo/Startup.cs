8a2b333e81181e9a034d3b804505c890bad55976
ernancythakkar <nancythakkar6@gmail.com>
2019-09-10 10:43:03 +0530

    first commit

---
+++ APIDemo/Startup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Newtonsoft.Json.Serialization;
+using Microsoft.Owin;
+using Owin;
+
+//[assembly: OwinStartup(typeof(APIDemo.Startup))]
+namespace APIDemo
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            config.Formatters.JsonFormatter.SerializerSettings.DateTimeZoneHandling = Newtonsoft.Json.DateTimeZoneHandling.Utc;
+            app.UseWebApi(config);
+
+        }
+    }
+}
